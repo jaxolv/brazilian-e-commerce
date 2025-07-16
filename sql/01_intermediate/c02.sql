0df1de1b45cf4692f1c27209da65541ae656ccf2
jaxolv <jackson.oliveira.dsa@gmail.com>
2025-07-16 09:21:22 -0300

    Implementando questão 02.sql

--- sql/01_intermediate/c02.sql
+++ sql/01_intermediate/c02.sql
@@ -1,10 +1,12 @@
 -- List the 10 products with the highest average freight value, considering only products with at least 5 sales.
 
 select
-    product_id,
-    round(sum(freight_value)/count(*), 2) as avg_freight_value
-from order_items
-group by product_id
+    oi.product_id,
+    product_category_name,
+    round(sum(oi.freight_value)/count(*), 2) as avg_freight_value
+from order_items oi
+join products p on oi.product_id = p.product_id
+group by oi.product_id, product_category_name
 having count(*) >= 5
 order by avg_freight_value desc
 limit 10
